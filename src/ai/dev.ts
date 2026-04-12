40623ef06cbd048c13d725dc67b2c4c483ee461e
Fernando Joana António <infortecmov@gmail.com>
2026-04-12 22:35:33 +0000

    fui em claro nao vamos usar Gemini, Genkit

--- src/ai/dev.ts
+++
@@ -1,6 +0,0 @@
-import { config } from 'dotenv';
-config();
-
-import '@/ai/flows/explain-technical-term.ts';
-import '@/ai/flows/generate-exercise-hint.ts';
-import '@/ai/flows/generate-lesson-content.ts';
