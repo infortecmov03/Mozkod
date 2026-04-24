cf78121d7659dde0726e4cb3b880479759ba740d
Fernando Joana António <codeworksmoz@gmail.com>
2026-04-24 17:55:20 +0000

    Try fixing this error: `Build Error: Parsing ecmascript source code fail

--- src/lib/curriculum/level-8-languages/01-html/theory/18-storage-persistence.ts
+++ src/lib/curriculum/level-8-languages/01-html/theory/18-storage-persistence.ts
@@ -76,7 +76,7 @@ await cache.addAll([
         <pre><code class="language-javascript">
 if (navigator.storage && navigator.storage.persist) {
   const isPersisted = await navigator.storage.persist();
-  console.log(`Armazenamento durável: ${isPersisted ? "ATIVO" : "NÃO"}`);
+  console.log(\`Armazenamento durável: \${isPersisted ? "ATIVO" : "NÃO"}\`);
 }
         </code></pre>
       </section>
